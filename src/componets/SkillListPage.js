460e2a2adc0c312f27dd8b794f7f39151ca6346f
YukiTome <tome.okinawa@gmail.com>
2023-02-06 18:30:36 +0900

    skilllistに追加

--- src/componets/SkillListPage.js
+++ src/componets/SkillListPage.js
@@ -2,6 +2,8 @@ import React from 'react'
 import reactImage from "../Images/react.png";
 import jsImage from "../Images/js.png";
 import pythonImage from "../Images/python.png";
+import cImage from "../Images/c.png";
+import javaImage from "../Images/java.png";
 
 const SkillListPage = () => {
   return (
@@ -26,19 +28,14 @@ const SkillListPage = () => {
                 <p>Pythonがつかえます</p>
                 </div>
                 <div class="col-md-4 services">
-                <img src={reactImage} alt="react"/>
-                <h4>React</h4>
-                <p>Reactがつかえます</p>
+                <img src={cImage} alt="c"/>
+                <h4>C言語</h4>
+                <p>C言語がつかえます</p>
                 </div>
                 <div class="col-md-4 services">
-                <img src={jsImage} alt="JavaScript"/>
-                <h4>HTML/CSS</h4>
-                <p>HTML/CSSがつかえます</p>
-                </div>
-                <div class="col-md-4 services">
-                <img src={pythonImage} alt="python"/>
-                <h4>Python</h4>
-                <p>Pythonがつかえます</p>
+                <img src={javaImage} alt="Java"/>
+                <h4>Java</h4>
+                <p>Javaがつかえます</p>
                 </div>
             </div>
             </div>
